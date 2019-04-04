83259908c1c2a2b2bf9ea2ba66ff9a786dba0ac8
John Maguire <john.maguire@creditkudos.com>
2019-04-04 10:23:11 +0100

    Add tests for CSR generation

---
+++ csr_test.go
@@ -0,0 +1,76 @@
+package eidas
+
+import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestKeyUsage(t *testing.T) {
+	Convey("key usage for QWAC", t, func() {
+		usage, err := keyUsageForType(QWACType)
+		So(err, ShouldBeNil)
+		So(usage, ShouldResemble, []x509.KeyUsage{
+			x509.KeyUsageDigitalSignature,
+		})
+	})
+	Convey("key usage for QSEAL", t, func() {
+		usage, err := keyUsageForType(QSEALType)
+		So(err, ShouldBeNil)
+		So(usage, ShouldResemble, []x509.KeyUsage{
+			x509.KeyUsageDigitalSignature,
+			x509.KeyUsageContentCommitment,
+		})
+	})
+}
+
+func TestExtendedKeyUsage(t *testing.T) {
+	Convey("extended key usage for QWAC", t, func() {
+		usage, err := extendedKeyUsageForType(QWACType)
+		So(err, ShouldBeNil)
+		So(usage, ShouldResemble, []asn1.ObjectIdentifier{
+			TLSWWWServerAuthUsage,
+			TLSWWWClientAuthUsage,
+		})
+	})
+}
+
+func TestBuildCSR(t *testing.T) {
+	Convey("CSR for QWAC", t, func() {
+		data, key, err := GenerateCSR("GB", "Foo Org", "Foo Org ID", "Foo Name", []string{"PSP_AI"}, QWACType)
+		So(err, ShouldBeNil)
+		So(key, ShouldNotBeNil)
+		csr, err := x509.ParseCertificateRequest(data)
+		So(err, ShouldBeNil)
+		So(csr.Subject.Country, ShouldResemble, []string{"GB"})
+		So(csr.Subject.Organization, ShouldResemble, []string{"Foo Org"})
+		names := csr.Subject.Names
+		So(names, shouldContainType, asn1.ObjectIdentifier{2, 5, 4, 97})
+		for _, name := range names {
+			if name.Type.Equal(asn1.ObjectIdentifier{2, 5, 4, 97}) {
+				So(name.Value, ShouldEqual, "Foo Org ID")
+			}
+		}
+	})
+}
+
+func shouldContainType(actual interface{}, expected ...interface{}) string {
+	attrs, ok := actual.([]pkix.AttributeTypeAndValue)
+	if !ok {
+		return "Expected []pkix.AttributeTypeAndValue"
+	}
+	for _, v := range attrs {
+		ex, ok := expected[0].(asn1.ObjectIdentifier)
+		if !ok {
+			return "Expected asn1.ObjectIdentifier"
+		}
+		if v.Type.Equal(ex) {
+			return ""
+		}
+	}
+	return fmt.Sprintf("Expected to find: %v", expected)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/creditkudos/eidas
 
 go 1.11
+
+require github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a
