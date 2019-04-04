2d051cac146320b771e638fb29008153f3c8ea2b
John Maguire <john.maguire@creditkudos.com>
2019-04-04 10:30:05 +0100

    Add tests for qcStatements extension

--- csr_test.go
+++ csr_test.go
@@ -48,11 +48,25 @@ func TestBuildCSR(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(csr.Subject.Country, ShouldResemble, []string{"GB"})
 		So(csr.Subject.Organization, ShouldResemble, []string{"Foo Org"})
+		So(csr.Subject.CommonName, ShouldEqual, "Foo Name")
+
 		names := csr.Subject.Names
 		So(names, shouldContainType, asn1.ObjectIdentifier{2, 5, 4, 97})
 		for _, name := range names {
 			if name.Type.Equal(asn1.ObjectIdentifier{2, 5, 4, 97}) {
 				So(name.Value, ShouldEqual, "Foo Org ID")
+			}
+		}
+
+		exts := csr.Extensions
+		So(exts, shouldContainId, asn1.ObjectIdentifier{1, 3, 6, 1, 5, 5, 7, 1, 3})
+		for _, ext := range exts {
+			if ext.Id.Equal(asn1.ObjectIdentifier{1, 3, 6, 1, 5, 5, 7, 1, 3}) {
+				roles, caName, caID, err := Extract(ext.Value)
+				So(err, ShouldBeNil)
+				So(roles, ShouldResemble, []string{"PSP_AI"})
+				So(caName, ShouldEqual, "Financial Conduct Authority")
+				So(caID, ShouldEqual, "GB-FCA")
 			}
 		}
 	})
@@ -69,6 +83,23 @@ func shouldContainType(actual interface{}, expected ...interface{}) string {
 			return "Expected asn1.ObjectIdentifier"
 		}
 		if v.Type.Equal(ex) {
+			return ""
+		}
+	}
+	return fmt.Sprintf("Expected to find: %v", expected)
+}
+
+func shouldContainId(actual interface{}, expected ...interface{}) string {
+	exts, ok := actual.([]pkix.Extension)
+	if !ok {
+		return "Expected []x509.Extension"
+	}
+	ex, ok := expected[0].(asn1.ObjectIdentifier)
+	if !ok {
+		return "Expected asn1.ObjectIdentifier"
+	}
+	for _, ext := range exts {
+		if ext.Id.Equal(ex) {
 			return ""
 		}
 	}
